be379629c3bb2c469ad2f8292e0caa64d705776a
JetA1100 <jeta1100@gmail.com>
2022-09-01 04:56:20 -0700

    Fixed errors in data insertion into database

--- process.php
+++ process.php
@@ -1,19 +1,15 @@
-<html>
-<body>
 <?php
-
-    echo "AHHHH";
-    include 'config.inc.php0';
-    $description = pg_escape_string($_POST['description']);
-    $date = pg_escape_string($_POST['date']);
-    $time = pg_escape_string($_POST['time']);
-    $rate = pg_escape_string($_POST['rate']);
+    include 'config.inc.php';
+    $description = $_POST['description'];
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+    $rate = $_POST['rate'];
     $query = "INSERT INTO timesheet (description, date, time, rate) VALUES('$description', '$date', '$time', '$rate')";
     $result = pg_query($query);
     if (! $result) {
         die('Invalid query:' . pg_last_error());
     }
-    echo "Data inserted Successfully!";
+    echo "Data inserted successfully";
+    header("Location: ./index.php");
+    exit;
 ?>
-</body>
-</html>

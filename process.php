831fb84e7f945932ef5300141fc248549e96b4f0
JetA1100 <jeta1100@gmail.com>
2022-09-01 02:07:55 -0700

    Testing sql query

--- process.php
+++ process.php
@@ -1,10 +1,10 @@
 <?php
     include 'config.inc.php';
     $description = pg_escape_string($_POST['description']);
-    $date = pg_escape_string($_POST['date']);
-    $time = pg_escape_string($_POST['time']);
-    $rate = pg_escape_string($_POST['rate']);
-    $query = "INSERT INTO timesheet (description, date, time, rate) VALUES('testing', '2022-01-02', '3', '4')";
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+    $rate = $_POST['rate'];
+    $query = "INSERT INTO timesheet (description, date, time, rate) VALUES('$description', '$date', '$time', '$rate')";
     $result = pg_query($query);
     if (! $result) {
         die('Invalid query:' . pg_last_error());

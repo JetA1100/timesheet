58da1c2bd7b712a589eb076618746e79987d43d1
JetA1100 <jeta1100@gmail.com>
2022-09-01 01:52:15 -0700

    Added hard coded values for testing purposes

--- process.php
+++ process.php
@@ -6,7 +6,7 @@
     $date = pg_escape_string($_POST['date']);
     $time = pg_escape_string($_POST['time']);
     $rate = pg_escape_string($_POST['rate']);
-    $query = "INSERT INTO timesheet (description, date, time, rate) VALUES('$description', '$date', '$time', '$rate')";
+    $query = "INSERT INTO timesheet (description, date, time, rate) VALUES('yellow', '2022-01-01', '5', '2')";
     $result = pg_query($query);
     if (! $result) {
         die('Invalid query:' . pg_last_error());

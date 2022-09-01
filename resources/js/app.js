540fe980ace1cdaf50029e7544d5e6f3b99f36e1
JetA1100 <jeta1100@gmail.com>
2022-09-01 04:47:56 -0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,8 @@ function calculate() {
         let cost = 0;
         for (let i = 0; i < data.rows.length; i++) {
             let row = data.rows[i];
-            let curTime = row.cells[3].children[0].value;
-            let curRate = row.cells[4].children[0].value;
+            let curTime = row.cells[2].children[0].value;
+            let curRate = row.cells[3].children[0].value;
             time += parseInt(curTime);
             cost += (parseInt(curTime) * parseInt(curRate));
         }

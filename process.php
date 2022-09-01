9e3c8470d5bf771041951a7e62f80489c0d8d6bc
JetA1100 <jeta1100@gmail.com>
2022-09-01 02:00:23 -0700

    readded hardcode for testing

--- process.php
+++ process.php
@@ -4,7 +4,7 @@
     $date = pg_escape_string($_POST['date']);
     $time = pg_escape_string($_POST['time']);
     $rate = pg_escape_string($_POST['rate']);
-    $query = "INSERT INTO timesheet (description, date, time, rate) VALUES('$description', '$date', '$time', '$rate')";
+    $query = "INSERT INTO timesheet (description, date, time, rate) VALUES('testing', '2022-01-02', '3', '4')";
     $result = pg_query($query);
     if (! $result) {
         die('Invalid query:' . pg_last_error());

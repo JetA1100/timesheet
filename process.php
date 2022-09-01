e529e49b48a4d2dcba555de50226958b0bd954ef
JetA1100 <jeta1100@gmail.com>
2022-09-01 03:49:28 -0700

    Update process.php

--- process.php
+++ process.php
@@ -3,13 +3,7 @@
     $sql = "SELECT * FROM timesheet";
     $result = pg_query($sql);
     $description = $_POST['description'];
-    while($row=pg_fetch_assoc($result)) {
-        if ($description == $row['description']) {
-            alert("Can not have duplicate descriptions");
-            //header("Location: ./index.php");
-        }
-    }
-    $date = $_POST['date'];
+     $_POST['date'];
     $time = $_POST['time'];
     $rate = $_POST['rate'];
     $query = "INSERT INTO timesheet (description, date, time, rate) VALUES('$description', '$date', '$time', '$rate')";

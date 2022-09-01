ea81a93352bca0f1304412037794fce4fb27ff40
JetA1100 <jeta1100@gmail.com>
2022-09-01 04:19:30 -0700

    Update

--- process.php
+++ process.php
@@ -1,17 +1,15 @@
 <?php
     include 'config.inc.php';
-
-    if($_POST['submit']) {
-        $description = $_POST['description'];
-        $date = $_POST['date'];
-        $time = $_POST['time'];
-        $rate = $_POST['rate'];
-        $query = "INSERT INTO timesheet (description, date, time, rate) VALUES('$description', '$date', '$time', '$rate')";
-        $result = pg_query($query);
-        if (! $result) {
-            die('Invalid query:' . pg_last_error());
-        }
-        echo "Data inserted Successfully!";
+    $sql = "SELECT * FROM timesheet";
+    $res = pg_query($sql);
+    $description = $_POST['description'];
+    $date = $_POST['date'];
+    $time = $_POST['time'];
+    $rate = $_POST['rate'];
+    $query = "INSERT INTO timesheet (description, date, time, rate) VALUES('$description', '$date', '$time', '$rate')";
+    $result = pg_query($query);
+    if (! $result) {
+        die('Invalid query:' . pg_last_error());
     }
     header("Location: ./index.php");
     exit;

a5880290878b585727271df7307942d4a1163392
JetA1100 <jeta1100@gmail.com>
2022-09-01 04:44:37 -0700

    Removed unnecessary functions

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,37 +1,3 @@
-function insertRow() {
-    let row = document.getElementById('insert').getElementsByTagName('tr');
-    let clone = row[row.length - 1].cloneNode(true);
-    let data = document.getElementById('data');
-    data.appendChild(clone);
-    clearInput();
-    setUpData();
-
-}
-
-function deleteRow() {
-    try {
-        let data = document.getElementById('data');
-        for (let i = 0; i < data.rows.length; i++) {
-            let row = data.rows[i];
-            let chkbox = row.cells[0].childNodes[0];
-            if (chkbox.checked) {
-                document.getElementById("timesheet").deleteRow(i+1);
-                i--;
-            }
-        }
-    } catch(e) {
-        alert(e);
-    }
-}
-
-function setUpData() {
-    $('#data td input, #data td textarea').prop('readonly', true);
-    $('#data th').each(function (i) {
-        let $td = $(this);
-        $td.html('<input class="select" type="checkbox" onclick="fixGrammar()">');
-    });
-}
-
 function calculate() {
     try {
         let data = document.getElementById('data');
@@ -50,26 +16,3 @@ function calculate() {
         alert(e);
     }
 }
-
-function fixGrammar() {
-    var count = document.querySelectorAll('input[type="checkbox"]:checked').length;
-    if (count < 2) {
-        document.getElementById('deleteBtn').textContent = "Delete Entry";
-    } else {
-        document.getElementById("deleteBtn").textContent = "Delete Entries";
-    }
-}
-
-function clearInput() {
-    $('#insert td input, #insert td textarea').val(null);
-}
-
-$('#timesheet-entry').submit(function(e) {
-    calculate();
-})
-
-$('#deleteBtn').click(function() {
-    ajaxcall();
-    deleteRow();
-    calculate();
-})
